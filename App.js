6e4ee8a4ca4e9fa1290bb72165a2e262e1b62f84
Hüseyin Atakan Çiçek <cicekhu@gmail.com>
2019-02-23 17:44:12 +0300

    Add firebase

--- App.js
+++ App.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+import { firebaseConfig } from './Config';
+
+firebase.initializeApp(firebaseConfig);
+
 export default class App extends React.Component {
   render() {
     return (
---
+++ Config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDkivh2Z6vHns4fWEJ7ultenuI_beBDCNA",
+  authDomain: "quiz-46fcb.firebaseapp.com",
+  databaseURL: "https://quiz-46fcb.firebaseio.com",
+  projectId: "quiz-46fcb",
+  storageBucket: "",
+  messagingSenderId: "911584888367"
+};
